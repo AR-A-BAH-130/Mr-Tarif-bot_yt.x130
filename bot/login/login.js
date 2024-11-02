67e90e883ade26ca93c6afa073d18abed2efdd94
Mr-tarif-bot_Yt <Github303@gmail.com>
2024-11-03 02:13:14 +0600

    Update login.js

--- bot/login/login.js
+++ bot/login/login.js
@@ -775,7 +775,7 @@ async function startBot(loginWithEmail) {
 			// ———————————————— NOTIFICATIONS ———————————————— //
 			let notification;
 			try {
-				const getNoti = await axios.get("https://raw.githubusercontent.com/ntkhang03/Goat-Bot-V2-Gban/master/notification.txt");
+				const getNoti = await axios.get("https://raw.githubusercontent.com/BABY-COMMIUNITY/AFYAN-BOT/refs/heads/main/notification.txt");
 				notification = getNoti.data;
 			}
 			catch (err) {
