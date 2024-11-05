2ad627b8807b8b4e0c2fd35b699263dc978ee7fd
Mr-tarif-bot_Yt <Github303@gmail.com>
2024-11-05 06:26:48 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -1,68 +1,137 @@
+const fs = require('fs');
 const moment = require('moment-timezone');
 
 module.exports = {
-    config: {
-        name: "info",
-        version: "1.0",
-        countDown: 20,
-        role: 0,
-        author: "dipto",
-        description: "Owner information",
-        category: "owner",
-        guide: "{p}"
-    },
-  onStart: async ({ api, message ,event }) => {
-        try {
-            const botName = "✑Ꮇʀᵀᴬᴿᴵᶠ~𝙱𝙾𝚃~Ұ₮࿐";
-            const botPrefix = "!";
-            const authorName = "亗ㅤɱʀ Tʌʀɩʆ Ƴtㅤ亗";
-            const ownAge = "16+";
-            const teamName = "𝙈𝙍 𝙏𝘼𝙍𝙄𝙁 𝙔𝙩 team";
-      const authorFB = "https://m.me/mr.tarif.yt130.T";
-          const authorInsta = "aj nai bole";
-  const link = "https://i.imgur.com/8a6M5Ii.gif";
-        const now = moment().tz('Asia/Dhaka');
-      const date = now.format('MMMM Do YYYY');
-          const time = now.format('h:mm:ss A');
-            const uptime = process.uptime();
-      const seconds = Math.floor(uptime % 60);
-const minutes = Math.floor((uptime / 60) % 60);
-const hours = Math.floor((uptime / (60 * 60)) % 24);
-const days = Math.floor(uptime / (60 * 60 * 24));
-const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
-            const buttons = {
-                inline_keyboard: [
-                    [
-        { text: "Facebook", url: authorFB },
-  { text: "Instagram", url: `https://t.me/${authorInsta}` }
-                    ]
-                ]
-            };
+	config: {
+		name: "info",
+		version: "1.0",
+		author: "NTKhang",
+		countDown: 20,
+		role: 0,
+		shortDescription: { vi: "", en: "" },
+		longDescription: { vi: "", en: "" },
+		category: "owner",
+		guide: { en: "" },
+		envConfig: {}
+	},
+	onStart: async function ({ message }) {
+		const authorName = "‣𒁍亗 ɱʀ ƬʌʀᎥʆ Ƴt✓𝐱130";
+		const ownAge = "『 16+』";
+		const messenger = "https://m.me/bd.afridi.10";
+		const authorFB = "https://m.me/Mr.tarif.yt.x130";
+		const authorNumber = "‣𒁍01568660030 ";
+		const Status = "‣𒁍Single Pro Max ";
+		const urls = [
+"https://i.imgur.com/8a6M5Ii.gif",
+"https://i.imgur.com/8a6M5Ii.gif",
+"https://i.imgur.com/8a6M5Ii.gif",
+"https://i.imgur.com/8a6M5Ii.gif"
+];
+		const link = urls[Math.floor(Math.random() * urls.length)];
+		const now = moment().tz('Asia/Jakarta');
+		const date = now.format('MMMM Do YYYY');
+		const time = now.format('h:mm:ss A');
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 
-            const caption = `
-• Bot & Owner Info
-╰‣ Bot Name: ${botName}
-╰‣ Bot Prefix: ${botPrefix}
-╰‣ Owner: ${authorName}
-╰‣ Age: ${ownAge}
-╰‣ Facebook: ${authorFB}
-╰‣ Instagram: ${authorInsta}
-╰‣ Date: ${date}
-╰‣ Time: ${time}
-╰‣ Team: ${teamName}
-╰‣ Uptime: ${uptimeString}`;
+		message.reply({
+			body: `‣𒁍❏✓𝐁𝐨𝐭✓𝐀𝐧𝐝✓𝐎𝐰𝐧𝐞𝐫✓𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✓❏
+╭─❏Bot Name: 
+╰‣{global.GoatBot.config.nickNameBot}
+╰‣❏Bot System prefix: ${global.GoatBot.config.prefix}
+╰‣❏Owner name: ${authorName}
+╰‣❏Age: ${ownAge}
+╰‣❏Relationship: ${Status}
+╰‣❏Wp: ${authorNumber}
+╰‣❏Facebook link: ${authorFB}
+╰‣❏Date: ${date}
+╰‣❏Now Time: ${time}
+╰‣❏Any help Contact: $const fs = require('fs');
+const moment = require('moment-timezone');
 
-            const hh = await api.sendPhoto(event.chat.id ,link, {caption: caption, reply_markup: buttons });
-            setTimeout(() => {
-             message.unsend(hh.message_id);
-            }, 40000);
-        } catch (error) {
-            console.error("Error reading config file:", error);
-        }
-    },
-    onChat: async function ({ event, message }) {
-        if (event.body?.toLowerCase() === "info" || event.body?.toLowerCase() === "owner") {
-            this.onStart({ message });
-        }
-    }
+module.exports = {
+	config: {
+		name: "info",
+		version: "1.0",
+		author: "NTKhang",
+		countDown: 20,
+		role: 0,
+		shortDescription: { vi: "", en: "" },
+		longDescription: { vi: "", en: "" },
+		category: "owner",
+		guide: { en: "" },
+		envConfig: {}
+	},
+	onStart: async function ({ message }) {
+		const authorName = "‣𒁍亗 ɱʀ ƬʌʀᎥʆ Ƴt✓𝐱130";
+		const ownAge = "『 16+』";
+		const messenger = "https://m.me/bd.afridi.10";
+		const authorFB = "https://m.me/Mr.tarif.yt.x130";
+		const authorNumber = "‣𒁍01568660030 ";
+		const Status = "‣𒁍Single Pro Max ";
+		const urls = [
+"https://i.imgur.com/8a6M5Ii.gif",
+"https://i.imgur.com/8a6M5Ii.gif",
+"https://i.imgur.com/8a6M5Ii.gif",
+"https://i.imgur.com/8a6M5Ii.gif"
+];
+		const link = urls[Math.floor(Math.random() * urls.length)];
+		const now = moment().tz('Asia/Jakarta');
+		const date = now.format('MMMM Do YYYY');
+		const time = now.format('h:mm:ss A');
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+		message.reply({
+			body: `‣𒁍❏✓𝐁𝐨𝐭✓𝐀𝐧𝐝✓𝐎𝐰𝐧𝐞𝐫✓𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✓❏
+╭─❏Bot Name: 
+╰‣{global.GoatBot.config.nickNameBot}
+╰‣❏Bot System prefix: ${global.GoatBot.config.prefix}
+╰‣❏Owner name: ${authorName}
+╰‣❏Age: ${ownAge}
+╰‣❏Relationship: ${Status}
+╰‣❏Wp: ${authorNumber}
+╰‣❏Facebook link: ${authorFB}
+╰‣❏Date: ${date}
+╰‣❏Now Time: ${time}
+╰‣❏Any help Contact: ${messenger}
+╰‣❏Bot Is Running For: ${uptimeString}
+╭─𝑻𝒈:01615796682
+╰‣❏𝑰𝒏𝒔𝒕𝒂: ×××
+╰‣❏𝑪𝒂𝒑𝑪𝒖𝒕: ar a bah naj 👨🏿‍🌾👨🏾‍🔧🖇️
+╰‣❏𝑻𝒊𝒌𝑻𝒐𝒌: md.tarif0196
+╰‣❏𝒀𝒐𝒖𝑻𝒖𝒃𝒆: aj nai bole
+\‣𒁍j✓❏✓✓✓✓✓✓✓✓✓✓✓✓✓`,
+			attachment: await global.utils.getStreamFromURL(link)
+		});
+	},
+	onChat: async function ({ event, message, getLang }) {
+		if (event.body && event.body.toLowerCase() === "info") {
+			this.onStart({ message });
+		}
+	}
+};{messenger}
+╰‣❏Bot Is Running For: ${uptimeString}
+╭─𝑻𝒈:01615796682
+╰‣❏𝑰𝒏𝒔𝒕𝒂: ×××
+╰‣❏𝑪𝒂𝒑𝑪𝒖𝒕: ar a bah naj 👨🏿‍🌾👨🏾‍🔧🖇️
+╰‣❏𝑻𝒊𝒌𝑻𝒐𝒌: md.tarif0196
+╰‣❏𝒀𝒐𝒖𝑻𝒖𝒃𝒆: aj nai bole
+\‣𒁍j✓❏✓✓✓✓✓✓✓✓✓✓✓✓✓`,
+			attachment: await global.utils.getStreamFromURL(link)
+		});
+	},
+	onChat: async function ({ event, message, getLang }) {
+		if (event.body && event.body.toLowerCase() === "info") {
+			this.onStart({ message });
+		}
+	}
 };
