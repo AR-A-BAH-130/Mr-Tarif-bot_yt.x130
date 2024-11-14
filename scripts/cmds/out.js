6dbfabf12691ab1a56a017c9b0d0d08254df4734
AR-A-BAH-130 <bukaxudara@gmail.com>
2024-11-14 13:49:27 +0600

    Update and rename Vag.js to out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 const request = require("request");
 module.exports = {
 	config: {
-		name: "vag",
+		name: "out",
 		aliases: ["vag"],
 		version: "1.0",
 		author: "ncs pro with TARIF",
