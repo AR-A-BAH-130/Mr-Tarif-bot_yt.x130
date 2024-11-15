ddbfc0dc5be6d17028c722133c068baabe71b791
AR-A-BAH-130 <bukaxudara@gmail.com>
2024-11-15 10:52:23 +0600

    Update tarif.js

--- scripts/cmds/tarif.js
+++ scripts/cmds/tarif.js
@@ -7,14 +7,14 @@ module.exports = {
 	 role: 0,
 	 shortDescription: "no prefix",
 	 longDescription: "no prefix",
-	 category: "no prefix",
+	 category: "𝗡𝗢 𝗣𝗥𝗘𝗙𝗜𝗫",
  },
 
  onStart: async function(){}, 
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "tarif") {
  return message.reply({
- body: " 「 🥰❥︎----ღ᭄_ᴛʜɪs ʙᴏᴛ ..\n❥︎----ღ᭄_ᴍᴀᴋᴇ ʙʏ ❞࿐🌴.\‣𒁍𝙈𝙍✓𝙏𝘼𝙍𝙄𝙁✓𝙔𝙩✓𝐱130✓ᴏᴋʜ ʙʏᴇ\n\n𝙱☺︎︎𝚃 owner\‣𒁍亗 ɱʀ ƬʌʀᎥʆ Ƴt✓𝐱130」",
+ body: " 「 🥰❥︎----ღ᭄_ᴛʜɪs ʙᴏᴛ ..\n❥︎----ღ᭄_ᴍᴀᴋᴇ ʙʏ ❞࿐🌴.\‣𒁍Ꮇʀ✓┳ꋬɾ꒐ᶠ✓𝐱130,ᴏᴋʜ ʙʏᴇ\n\n𝙱☺︎︎𝚃 owner\‣𒁍亗 ɱʀ ƬʌʀᎥʆ Ƴt✓𝐱130」",
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/tKgVilB.mp4")
  });
  }
